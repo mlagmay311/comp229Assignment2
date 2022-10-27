6131bc330af86628cd4c8e25852bec73fe335491
mlagmay311 <mlagmay@my.centennialcollege.ca>
2022-10-27 15:02:51 -0700

    Updated Global database

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  URI: "mongodb://127.0.0.1:27017/ContactList",
- 
+  //URI: "mongodb://127.0.0.1:27017/ContactList",
+  URI:"mongodb+srv://ekomahmen:ekomahmen@cluster0.fh2hwzf.mongodb.net/ContactList?retryWrites=true&w=majority"
 };
